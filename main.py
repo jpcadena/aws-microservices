8d0494f939c8761817ba3d525309ec6945215057
Juan Pablo Cadena Aguilar <jpcadena@espol.edu.ec>
2024-12-28 15:47:02 -0500

    🚀 Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+Main script for aws-microservices project
+"""
+
+def main() -> None:
+    """
+    Main method
+
+    :return: None
+    :rtype: NoneType
+    """
+    print("Hello from aws-microservices!")
+
+
+if __name__ == "__main__":
+    main()
